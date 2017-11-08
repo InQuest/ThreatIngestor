354624fd9fcafd56cad1be3f0c3f3ebac07412d9
rshipp <github@rshipp.com>
2017-11-08 11:33:36 -0600

    Use api dedup support

--- operators/threatkb.py
+++ operators/threatkb.py
@@ -26,50 +26,30 @@ class ThreatKB(Operator):
 
     def handle_domain(self, domain):
         """Handle a single domain"""
-        existing = self.api.get('c2dns')
-        if str(domain) in [dns['domain_name'].encode('utf-8') for dns in existing]:
-            # post comment
-            self.api.create('comments', {
-                    'comment': u"{c}\n\n{u}".format(c=domain.reference_text, u=domain.reference_link),
-                    'entity_id': [dns['id'] for dns in existing if dns['domain_name'].encode('utf-8') == str(domain)][:1],
-                    'entity_type': 2,
-                })
-        else:
-            # post c2 domain
-            self.api.create('c2dns', {
-                    'domain_name': str(domain),
-                    'match_type': '',
-                    'reference_link': domain.reference_link,
-                    'description': domain.reference_text,
-                    'expiration_type': '',
-                    'state': {'state': self.state},
-                    'tags': [],
-                })
+        self.api.create('c2dns', {
+                'domain_name': str(domain),
+                'match_type': '',
+                'reference_link': domain.reference_link,
+                'description': domain.reference_text,
+                'expiration_type': '',
+                'state': {'state': self.state},
+                'tags': [],
+            })
 
     def handle_ipaddress(self, ipaddress):
         """Handle a single IP address"""
-        existing = self.api.get('c2ips')
-        if str(ipaddress) in [addr['ip'] for addr in existing]:
-            # post comment
-            self.api.create('comments', {
-                    'comment': u"{c}\n\n{u}".format(c=ipaddress.reference_text, u=ipaddress.reference_link),
-                    'entity_id': [addr['id'] for addr in existing if addr['ip'] == str(ipaddress)][0],
-                    'entity_type': 3,
-                })
-        else:
-            # post c2 ip
-            self.api.create('c2ips', {
-                    'ip': str(ipaddress),
-                    'reference_link': ipaddress.reference_link,
-                    'description': ipaddress.reference_text,
-                    'expiration_type': '',
-                    'state': {'state': self.state},
-                    'asn': '',
-                    'country': '',
-                    'city': '',
-                    'st': '',
-                    'tags': [],
-                })
+        self.api.create('c2ips', {
+                'ip': str(ipaddress),
+                'reference_link': ipaddress.reference_link,
+                'description': ipaddress.reference_text,
+                'expiration_type': '',
+                'state': {'state': self.state},
+                'asn': '',
+                'country': '',
+                'city': '',
+                'st': '',
+                'tags': [],
+            })
 
     def handle_yarasignature(self, yarasignature):
         """Handle a single Yara signature"""
