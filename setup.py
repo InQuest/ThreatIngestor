b58df41a427ba2454ec43d67bb4eaa0bef9aeb8c
rshipp <github@rshipp.com>
2018-08-08 13:29:39 -0500

    Update Python support in settings.py and Travis config

--- setup.py
+++ setup.py
@@ -37,7 +37,9 @@ setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3 :: Only'
         'Topic :: Security',
         'Topic :: Internet',
     ],
