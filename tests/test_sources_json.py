e8158e5ca6c8a145f39180ca452c804517ec47d1
rshipp <github@rshipp.com>
2019-02-08 13:34:36 -0600

    Extend JSON source, add tests

--- tests/test_sources_json.py
+++ tests/test_sources_json.py
@@ -12,13 +12,44 @@ import threatingestor.exceptions
 class TestJSON(unittest.TestCase):
 
     def setUp(self):
-        self.abs_json = threatingestor.sources.abstract_json.AbstractPlugin("name", [],"")
+        self.abs_json = threatingestor.sources.abstract_json.AbstractPlugin("name", [], "")
 
     def test_run(self):
-        
-        content = json.loads('{"testKey": "testValue"}')
-        self.abs_json.get_content = lambda x: content
+        content_list = [{"testKey": "testValue"}]
+        self.abs_json.get_objects = lambda x: content_list
 
         saved_state, artifact_list = self.abs_json.run(None)
 
         self.assertEqual(saved_state, None)
+
+    def test_run_uses_reference_path_if_possible(self):
+        content_list = [{"testContent": "http://example.com", "testRef": "myReference"}]
+        abs_json = threatingestor.sources.abstract_json.AbstractPlugin("name", ["testContent"], "testRef")
+        abs_json.get_objects = lambda x: content_list
+
+        saved_state, artifact_list = abs_json.run(None)
+
+        self.assertEqual(3, len(artifact_list))
+        for artifact in artifact_list:
+            self.assertEqual('myReference', artifact.reference_link)
+
+    def test_run_uses_name_if_reference_path_not_found(self):
+        content_list = [{"testContent": "http://example.com", "testRef": "myReference"}]
+        abs_json = threatingestor.sources.abstract_json.AbstractPlugin("name", ["testContent"], "badRef")
+        abs_json.get_objects = lambda x: content_list
+
+        saved_state, artifact_list = abs_json.run(None)
+
+        self.assertEqual(3, len(artifact_list))
+        for artifact in artifact_list:
+            self.assertEqual('name', artifact.reference_link)
+
+    def test_run_uses_name_if_reference_path_not_found(self):
+        content_list = [{"testContent": "http://example.com", "testRef": "myReference"}]
+        abs_json = threatingestor.sources.abstract_json.AbstractPlugin("name", ["testContent"], "testRef")
+        abs_json.get_objects = lambda x: content_list
+
+        saved_state, artifact_list = abs_json.run(None)
+
+        self.assertEqual(3, len(artifact_list))
+        self.assertIn('http://example.com', [str(x) for x in artifact_list])
--- threatingestor/sources/abstract_json.py
+++ threatingestor/sources/abstract_json.py
@@ -1,22 +1,24 @@
-from jsonpath_rw import jsonpath, parse
-
 import json
+
+import jsonpath_rw
 
 from threatingestor.sources import Source
 
 class AbstractPlugin(Source):
 
-    def __init__(self, name, paths, reference):
+    def __init__(self, name, paths, reference=None):
         """Args should be url, auth, etc, whatever is needed to set up the object.
 
         The name argument is required for all Source plugins, and is used internally.
         """
         self.name = name
-        self.paths=paths
-        self.reference= reference
+        # Parse paths into path expressions.
+        self.path_expressions = [jsonpath_rw.parse(path) for path in paths]
+        # Try to parse reference as a path, fall back to name.
+        self.reference = jsonpath_rw.parse(reference) if reference else name
 
-    def get_content(self):
-        """Return dict or list of raw content to process.
+    def get_objects(self, saved_state):
+        """Produce an iterable of dict or list objectcs containing raw content to process.
 
         Override in child class."""
         raise NotImplementedError()
@@ -24,12 +26,20 @@ class AbstractPlugin(Source):
     def run(self, saved_state):
         """Run and return (saved_state, list(Artifact))"""
         artifact_list = []
-        for path in self.paths:
-            jsonpath_expr = parse(path)
-            matches = jsonpath_expr.find(self.get_content())
 
-            for match in matches:
-                artifact_list += self.process_element(match.value, self.reference, include_nonobfuscated=True)
+        for content in self.get_objects(saved_state):
+            # Iterate over each piece of "content", extracting from every path of interest.
+            for path_expression in self.path_expressions:
+                matches = path_expression.find(content)
 
+                # Try to get a single reference, fall back to name again.
+                try:
+                    reference = self.reference.find(content)[0].value
+                except IndexError:
+                    reference = self.name
+
+                # Extract artifacts.
+                for match in matches:
+                    artifact_list += self.process_element(match.value, reference, include_nonobfuscated=True)
 
         return saved_state, artifact_list
