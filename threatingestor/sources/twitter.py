14944cd763cee06fa95b81fa0fa56a63b8ef71c8
rshipp <github@rshipp.com>
2019-03-06 11:10:28 -0600

    Update Twitter source docs

--- threatingestor/sources/twitter.py
+++ threatingestor/sources/twitter.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
+
 import twitter
+
 
 from threatingestor.sources import Source
 
@@ -14,18 +16,18 @@ class Plugin(Source):
         self.name = name
         self.api = twitter.Twitter(auth=twitter.OAuth(token, token_key, con_secret, con_secret_key))
 
-        # let the user decide whether to include non-obfuscated URLs or not.
+        # Let the user decide whether to include non-obfuscated URLs or not.
         self.include_nonobfuscated = not defanged_only
 
-        # forward kwargs.
-        # NOTE: no validation is done here, so if the config is wrong, expect bad results.
+        # Forward kwargs.
+        # NOTE: No validation is done here, so if the config is wrong, expect bad results.
         self.kwargs = kwargs
 
-        # decide which endpoint to use based on passed arguments.
-        # if slug and owner_screen_name, use List API.
-        # if screen_name or user_id, use User Timeline API.
-        # if q is set, use Search API.
-        # otherwise, default to mentions API.
+        # Decide which endpoint to use based on passed arguments.
+        # If slug and owner_screen_name, use List API.
+        # If screen_name or user_id, use User Timeline API.
+        # If q is set, use Search API.
+        # Otherwise, default to mentions API.
         self.endpoint = self.api.statuses.mentions_timeline
         if kwargs.get('slug') and kwargs.get('owner_screen_name'):
             self.endpoint = self.api.lists.statuses
@@ -34,22 +36,22 @@ class Plugin(Source):
         elif kwargs.get('q'):
             self.endpoint = self.api.search.tweets
 
-    def run(self, saved_state):
 
-        # modify kwargs to insert since_id
+    def run(self, saved_state):
+        # Modify kwargs to insert since_id.
         if saved_state:
             self.kwargs['since_id'] = saved_state
 
-        # pull new tweets
+        # Pull new tweets.
         try:
             response = self.endpoint(**self.kwargs)
         except twitter.api.TwitterHTTPError as e:
-            # API error; log and return early
+            # API error; log and return early.
             print("Twitter API Error: {e}".format(e=e))
 
             return saved_state, []
 
-        # correctly handle responses from different endpoints
+        # Correctly handle responses from different endpoints.
         try:
             tweet_list = response['statuses']
         except TypeError:
@@ -63,18 +65,18 @@ class Plugin(Source):
         } for s in tweet_list]
 
         artifacts = []
-        # traverse in reverse, old to new
+        # Traverse in reverse, old to new.
         tweets.reverse()
         for tweet in tweets:
-            # expand t.co links
+            # Expand t.co links.
             for url in tweet['entities'].get('urls', []):
                 try:
                     tweet['content'] = tweet['content'].replace(url['url'], url['expanded_url'])
                 except KeyError:
-                    # no url/expanded_url, continue without expanding
+                    # No url/expanded_url, continue without expanding.
                     pass
 
-            # process tweet
+            # Process tweet.
             saved_state = tweet['id']
             artifacts += self.process_element(tweet['content'],
                                               TWEET_URL.format(user=tweet['user'], id=tweet['id']),
