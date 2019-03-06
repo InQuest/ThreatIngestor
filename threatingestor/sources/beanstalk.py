883a28d0eabb0469b523a44bd8fa233be463e770
rshipp <github@rshipp.com>
2019-03-06 13:47:43 -0600

    Batch inputs in beanstalk source

--- threatingestor/sources/beanstalk.py
+++ threatingestor/sources/beanstalk.py
@@ -11,6 +11,9 @@ except ImportError:
     raise DependencyError("Dependency greenstalk required for Beastalk operator is not installed")
 
 
+BATCH_SIZE = 10
+
+
 class Plugin(abstract_json.AbstractPlugin):
     """Source for Beanstalk work queue."""
     def __init__(self, name, host, port, queue_name, paths, reference=None):
@@ -25,12 +28,15 @@ class Plugin(abstract_json.AbstractPlugin):
         # Just do one job at a time, so we don't have to worry about batching
         # or TTL timeouts.
         try:
-            for message in self.queue.reserve(timeout=1):
+            batch_count = 0
+            while batch_count < BATCH_SIZE:
+                message = self.queue.reserve(timeout=1):
                 job = json.loads(message.body)
                 job_list.append(job)
 
                 # Let the queue know that the message is processed.
                 self.queue.delete(message)
+                batch_count += 1
 
         except greenstalk.TimedOutError:
             # No more jobs.
