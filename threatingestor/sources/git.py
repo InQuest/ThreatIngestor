6a747304b711867992529f8e529467ee00f88a72
rshipp <github@rshipp.com>
2019-03-06 11:06:48 -0600

    Update Git source

--- threatingestor/sources/git.py
+++ threatingestor/sources/git.py
@@ -3,7 +3,9 @@ import io
 import sys
 import subprocess
 
+
 from threatingestor.sources import Source
+
 
 YARA_FILE_EXTS = [
     '.rule',
@@ -12,6 +14,7 @@ YARA_FILE_EXTS = [
     '.rules',
 ]
 
+
 class Plugin(Source):
 
     def __init__(self, name, url, local_path):
@@ -19,13 +22,14 @@ class Plugin(Source):
         self.url = url
         self.local_path = local_path
 
+
     def run(self, saved_state):
-        # initialize
+        # Initialize.
         new_hash = saved_state
         all_filenames = []
 
         if saved_state:
-            # pull and diff to get filenames
+            # Pull and diff to get filenames.
             try:
                 _git_pull(self.local_path)
                 new_hash = _git_latest_hash(self.local_path)
@@ -34,7 +38,7 @@ class Plugin(Source):
                 sys.stderr.write("error with git pull from {path}: {e}\n".format(path=self.local_path, e=e))
 
         else:
-            # first time, we try cloning the repo and look at all files
+            # First run, we try cloning the repo and look at all files.
             try:
                 _git_clone(self.url, self.local_path)
                 new_hash = _git_latest_hash(self.local_path)
@@ -44,11 +48,11 @@ class Plugin(Source):
                                                                                          path=self.local_path,
                                                                                          e=e))
 
-        # if not modified or any errors, return immediately
+        # If not modified or any errors, return immediately.
         if saved_state == new_hash:
             return saved_state, []
 
-        # otherwise, process yara files
+        # Otherwise, process YARA files.
         artifact_list = []
         for filename in all_filenames:
             if any([filename.endswith(x) for x in YARA_FILE_EXTS]):
@@ -59,9 +63,13 @@ class Plugin(Source):
 
 
 def _git_cmd(args):
-    # may raise subprocess.CalledProcessError
+    """Call a git command.
+
+    :raises: subprocess.CalledProcessError
+    """
     git_cmdline = ['git'] + args
     return subprocess.check_output(git_cmdline).decode('utf-8')
+
 
 def _git_cmd_chdir(path, args):
     cwd = os.getcwd()
@@ -70,17 +78,22 @@ def _git_cmd_chdir(path, args):
     os.chdir(cwd)
     return output
 
+
 def _git_clone(remote, local_path):
     return _git_cmd(['clone', '--', remote, local_path])
+
 
 def _git_latest_hash(local_path):
     return _git_cmd_chdir(local_path, ['rev-parse', 'HEAD']).strip()
 
+
 def _git_pull(local_path):
     return _git_cmd_chdir(local_path, ['pull'])
 
+
 def _git_diff_names(local_path, prev_hash):
     return _git_cmd_chdir(local_path, ['diff', '--name-only', '--', prev_hash])
+
 
 def _git_ls_files(local_path):
     return _git_cmd_chdir(local_path, ['ls-files'])
