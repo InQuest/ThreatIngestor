ef70d004c3266977c413ad48205635c2b74f0e6d
rshipp <github@rshipp.com>
2019-03-06 10:52:20 -0600

    Update Ingestor docs

--- threatingestor/__init__.py
+++ threatingestor/__init__.py
@@ -1,53 +1,64 @@
 import sys
 import time
 
+
 import threatingestor.config
 import threatingestor.state
 from threatingestor.exceptions import IngestorError
 
 
 class Ingestor:
+    """ThreatIngestor main work logic.
 
+    Handles reading the config file, calling sources, maintaining state, and
+    sending artifacts to operators.
+    """
     def __init__(self, config_file):
-        # load config
+        # Load config.
         try:
             self.config = config.Config(config_file)
         except IngestorError as e:
-            # error loading config
+            # Error loading config.
             sys.stderr.write(e)
             sys.exit(1)
 
-        # load state db
+        # Load state DB.
         try:
             self.statedb = threatingestor.state.State(self.config.state_path())
         except (OSError, IOError) as e:
-            # error loading state db
+            # Error loading state DB.
             sys.stderr.write(e)
             sys.exit(1)
 
-        # instantiate plugins
-        self.sources = dict([(name, source(**kwargs)) for name, source, kwargs in self.config.sources()])
-        self.operators = dict([(name, operator(**kwargs)) for name, operator, kwargs in self.config.operators()])
+        # Instantiate plugins.
+        self.sources = {name: source(**kwargs)
+                        for name, source, kwargs in self.config.sources()}
+        self.operators = {name: operator(**kwargs)
+                          for name, operator, kwargs in self.config.operators()}
+
 
     def run(self):
+        """Run once, or forever, depending on config."""
         if self.config.daemon():
             self.run_forever()
         else:
             self.run_once()
 
+
     def run_once(self):
+        """Run each source once, passing artifacts to each operator."""
         for source in self.sources:
-            # run the source to collect artifacts
+            # Run the source to collect artifacts.
             try:
                 saved_state, artifacts = self.sources[source].run(self.statedb.get_state(source))
             except Exception as e:
                 sys.stderr.write("Unknown error in source {s}: {e}\n".format(s=source, e=e))
                 continue
 
-            # save the source state
+            # Save the source state.
             self.statedb.save_state(source, saved_state)
 
-            # process artifacts with each operator
+            # Process artifacts with each operator.
             for operator in self.operators:
                 try:
                     self.operators[operator].process(artifacts)
@@ -55,18 +66,23 @@ class Ingestor:
                     sys.stderr.write("Unknown error in operator {o}: {e}\n".format(o=operator, e=e))
                     continue
 
+
     def run_forever(self):
+        """Run forever, sleeping for the configured interval between each run."""
         while True:
             self.run_once()
             time.sleep(self.config.sleep())
 
+
 def main():
+    """CLI entry point, uses sys.argv directly."""
     if len(sys.argv) < 2:
         print("You must specify a config file")
         sys.exit(1)
 
     app = Ingestor(sys.argv[1])
     app.run()
+
 
 if __name__ == "__main__":
     main()
