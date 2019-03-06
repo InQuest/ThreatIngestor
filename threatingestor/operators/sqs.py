b7e93fe5df3e11e996df158a3cc132ef2b5a9fe9
rshipp <github@rshipp.com>
2019-03-06 11:15:25 -0600

    Update SQS operator

--- threatingestor/operators/sqs.py
+++ threatingestor/operators/sqs.py
@@ -1,20 +1,22 @@
 import json
 
-from threatingestor.exceptions import DependencyError
-import threatingestor.artifacts
-from threatingestor.operators import abstract_json
 
 try:
     import boto3
 except ImportError:
     raise DependencyError("Dependency boto3 required for SQS operator is not installed")
 
-class Plugin(abstract_json.AbstractPlugin):
-    """Operator for Amazon SQS"""
 
+from threatingestor.exceptions import DependencyError
+import threatingestor.artifacts
+from threatingestor.operators import abstract_json
+
+
+class Plugin(abstract_json.AbstractPlugin):
+    """Operator for Amazon SQS."""
     def __init__(self, aws_access_key_id, aws_secret_access_key, aws_region, queue_name,
             artifact_types=None, filter_string=None, allowed_sources=None, **kwargs):
-        """SQS operator"""
+        """SQS operator."""
         self.sqs = boto3.client('sqs', region_name=aws_region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         self.queue_url = self.sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
 
@@ -26,8 +28,9 @@ class Plugin(abstract_json.AbstractPlugin):
             threatingestor.artifacts.URL,
         ]
 
+
     def _put(self, content):
-        """Send content to an SQS queue"""
+        """Send content to an SQS queue."""
         return self.sqs.send_message(
                 QueueUrl=self.queue_url,
                 DelaySeconds=0,
