b15cc4dadbb527a2701ad84bc753c3665e9a4a7c
rshipp <github@rshipp.com>
2019-03-06 13:24:52 -0600

    Fix fswatcher

--- threatingestor/extras/fswatcher.py
+++ threatingestor/extras/fswatcher.py
@@ -17,7 +17,6 @@ class FSWatcher(
 
     Send contents of the changed rule files to the queue.
     """
-
     # Only match YARA rules.
     patterns = ["*.yar", "*.yara", "*.rule", "*.rules"]
 
@@ -25,10 +24,14 @@ class FSWatcher(
         """Handle a file event."""
         with io.open(event.src_path, 'r') as rule_source:
             rule_content = rule_source.read()
-            self.queue.write_one(rule_content)
+            self.queue.write_one({
+                'rules': rule_content,
+            })
+
 
     def on_modified(self, event):
         self.process(event)
+
 
     def on_created(self, event):
         self.process(event)
