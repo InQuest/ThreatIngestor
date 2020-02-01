f77db31ebb9f5007651644a3d4ad369ff8299e8f
Manabu Niseki <manabu.niseki@gmail.com>
2020-02-02 07:41:02 +0900

    Update MISP operator
    Catch up to the latest PyMISP & make enable to aggregate attributes to an event (group by a reference link)

--- threatingestor/operators/misp.py
+++ threatingestor/operators/misp.py
@@ -9,21 +9,24 @@ from threatingestor.exceptions import DependencyError
 try:
     import pymisp
 except ImportError:
-    raise DependencyError("Dependency PyMISP required for MISP operator is not installed")
+    raise DependencyError(
+        "Dependency PyMISP required for MISP operator is not installed")
 
 
 class Plugin(Operator):
     """Operator for MISP."""
+
     def __init__(self, url, key, ssl=True, tags=None, artifact_types=None, filter_string=None, allowed_sources=None):
         """MISP operator."""
-        self.api = pymisp.PyMISP(url, key, ssl, 'json')
+        self.api = pymisp.ExpandedPyMISP(url, key, ssl)
         if tags:
             self.tags = tags
         else:
             self.tags = ['type:OSINT']
         self.event_info = 'ThreatIngestor Event: {source_name}'
 
-        super(Plugin, self).__init__(artifact_types, filter_string, allowed_sources)
+        super(Plugin, self).__init__(
+            artifact_types, filter_string, allowed_sources)
         self.artifact_types = artifact_types or [
             threatingestor.artifacts.Domain,
             threatingestor.artifacts.Hash,
@@ -32,69 +35,102 @@ class Plugin(Operator):
             threatingestor.artifacts.YARASignature,
         ]
 
-
     def handle_artifact(self, artifact):
         """Operate on a single artifact."""
-        if isinstance(artifact, threatingestor.artifacts.Domain):
-            self.handle_domain(artifact)
-        if isinstance(artifact, threatingestor.artifacts.Hash):
-            self.handle_hash(artifact)
-        elif isinstance(artifact, threatingestor.artifacts.IPAddress):
-            self.handle_ipaddress(artifact)
-        if isinstance(artifact, threatingestor.artifacts.URL):
-            self.handle_url(artifact)
-        elif isinstance(artifact, threatingestor.artifacts.YARASignature):
-            self.handle_yarasignature(artifact)
+        event = self._find_or_create_event(artifact)
 
+        if isinstance(artifact, threatingestor.artifacts.Domain):
+            event = self.handle_domain(event, artifact)
+        if isinstance(artifact, threatingestor.artifacts.Hash):
+            event = self.handle_hash(event, artifact)
+        elif isinstance(artifact, threatingestor.artifacts.IPAddress):
+            eveng = self.handle_ipaddress(event, artifact)
+        if isinstance(artifact, threatingestor.artifacts.URL):
+            event = self.handle_url(event, artifact)
+        elif isinstance(artifact, threatingestor.artifacts.YARASignature):
+            event = self.handle_yarasignature(event, artifact)
+
+        self._update_or_create_event(event)
+
+    def _update_or_create_event(self, event):
+        """Update or create an event for the artifact."""
+        event_dict = event.to_dict()
+        attributes = event_dict.get("Attribute", [])
+        if len(attributes) == 0:
+            return
+
+        if event_dict.get("uuid") is None:
+            self.api.add_event(event)
+        else:
+            self.api.update_event(event)
+
+    def _find_or_create_event(self, artifact):
+        """Find or create an event for the artifact."""
+        event = self._find_event(artifact)
+        if event is not None:
+            return event
+
+        return self._create_event(artifact)
+
+    def _find_event(self, artifact):
+        """Find an event which has the same refetrence link, return an Event object."""
+        events = self.api.search(
+            "events",
+            limit=1,
+            type_attribute="link",
+            value=artifact.reference_link,
+            pythonify=True
+        )
+        if len(events) == 1:
+            return events[0]
+
+        return None
 
     def _create_event(self, artifact):
         """Create an event in MISP, return an Event object."""
-        event = self.api.new_event(info=self.event_info.format(
-            source_name=artifact.source_name))
+        event = pymisp.MISPEvent()
+        event.info = self.event_info.format(source_name=artifact.source_name)
 
         # Add tags.
         for tag in self.tags:
-            self.api.add_tag(event, tag)
+            event.add_tag(tag)
 
         # Add references.
-        self.api.add_internal_link(event, artifact.reference_link)
-        self.api.add_internal_text(event, artifact.reference_text)
-        self.api.add_internal_other(event, f'source:{artifact.source_name}')
+        if artifact.reference_link != "":
+            event.add_attribute("link", artifact.reference_link)
+        if artifact.reference_text != "":
+            event.add_attribute("text", artifact.reference_text)
+        if artifact.source_name != "":
+            event.add_attribute("other",  f'source:{artifact.source_name}')
 
         return event
 
-
-    def handle_domain(self, domain):
+    def handle_domain(self, event, domain):
         """Handle a single domain."""
-        event = self._create_event(domain)
-        self.api.add_domain(event, str(domain))
+        event.add_attribute("domain", str(domain))
+        return event
 
-    def handle_hash(self, hash_):
+    def handle_hash(self, event, hash_):
         """Handle a single hash."""
         if hash_.hash_type() == hash_.MD5:
-            event = self._create_event(hash_)
-            self.api.add_hashes(event, md5=str(hash_))
+            event.add_attribute("md5", str(hash_))
         elif hash_.hash_type() == hash_.SHA1:
-            event = self._create_event(hash_)
-            self.api.add_hashes(event, sha1=str(hash_))
+            event.add_attribute("sha1", str(hash_))
         elif hash_.hash_type() == hash_.SHA256:
-            event = self._create_event(hash_)
-            self.api.add_hashes(event, sha256=str(hash_))
+            event.add_attribute("sha256", str(hash_))
+        return event
 
-
-    def handle_ipaddress(self, ipaddress):
+    def handle_ipaddress(self, event, ipaddress):
         """Handle a single IP address."""
-        event = self._create_event(ipaddress)
-        self.api.add_ipdst(event, str(ipaddress))
+        event.add_attribute("ip-dst", str(ipaddress))
+        return event
 
-
-    def handle_url(self, url):
+    def handle_url(self, event, url):
         """Handle a single URL."""
-        event = self._create_event(url)
-        self.api.add_url(event, str(url))
+        event.add_attribute("url", str(url))
+        return event
 
-
-    def handle_yarasignature(self, yarasignature):
+    def handle_yarasignature(self, event, yarasignature):
         """Handle a single YARA signature."""
-        event = self._create_event(yarasignature)
-        self.api.add_yara(event, str(yarasignature))
+        event.add_attribute("yara", str(yarasignature))
+        return event
