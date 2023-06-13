cf90a6acf85b3154cbb8c82fa005a2ec1e5d1f47
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-06-13 18:49:42 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if not isinstance(extra_requires, list):
 
 setup(
     name='threatingestor',
-    version='1.1.1',
+    version='1.2.0',
     include_package_data=True,
     install_requires=requires,
     extras_require={
