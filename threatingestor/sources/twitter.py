e120b2bd4589220f3006b768f15931d79ca2ce44
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-08-31 17:15:15 -0500

    Migration complete
    Still need to rewrite test cases for new API.

--- threatingestor/sources/twitter.py
+++ threatingestor/sources/twitter.py
@@ -1,22 +1,27 @@
 from __future__ import absolute_import
 
-import twitter
+import twitter, requests
 from loguru import logger
 
 from threatingestor.sources import Source
 
 class Plugin(Source):
 
-    def __init__(self, name, api_key, api_secret_key, access_token, access_token_secret, bearer_token=None, defanged_only=True, **kwargs):
+    def __init__(self, name, api_key, api_secret_key, access_token, access_token_secret, bearer_token, defanged_only=True, **kwargs):
         self.name = name
+        self.bearer_token = bearer_token
 
         """
         Depending on the endpoint being used, we need to modify the type of authentiation being used
         How to identity the type of authentication = https://developer.twitter.com/en/docs/authentication/overview
         """
         self.auth = {
-            "OAuth": twitter.Twitter(auth=twitter.OAuth(access_token, access_token_secret, api_key, api_secret_key), api_version="1.1"),
-            "OAuth2": twitter.Twitter(auth=twitter.OAuth2(bearer_token=bearer_token), api_version="2", format="")
+            "OAuth": twitter.Twitter(auth=twitter.OAuth(token=access_token,
+                                                        token_secret=access_token_secret,
+                                                        consumer_key=api_key,
+                                                        consumer_secret=api_secret_key),
+                                                        api_version="1.1"),
+            "OAuth2": twitter.Twitter(auth=twitter.OAuth2(bearer_token=self.bearer_token), api_version="2", format="")
         }
 
         # Let the user decide whether to include non-obfuscated URLs or not.
@@ -25,38 +30,55 @@ class Plugin(Source):
         # Forward kwargs.
         # NOTE: No validation is done here, so if the config is wrong, expect bad results.
         self.kwargs = kwargs
-
-        # Decide which endpoint to use based on passed arguments.
-        # If slug and owner_screen_name, use List API.
-        # If screen_name or user_id, use User Timeline API.
-        # If query is set, use Search API.
-        # Otherwise, default to mentions API.
-        self.endpoint = self.auth['OAuth2'].statuses.mentions_timeline
-        if kwargs.get('id'):
-            self.endpoint = self.auth['OAuth'].lists.statuses
-        elif kwargs.get('screen_name') or kwargs.get('user_id'):
-            self.endpoint = self.auth['OAuth'].statuses.user_timeline
-        elif kwargs.get('query'):
-            self.endpoint = self.auth['OAuth2'].tweets.search.recent
+        self.endpoint = None
 
     def run(self, saved_state):
         # Modify kwargs to insert since_id.
         if saved_state:
             self.kwargs['since_id'] = saved_state
 
-        # Pull new tweets.
-        try:
-            response = self.endpoint(**self.kwargs)
-        except twitter.api.TwitterHTTPError as e:
-            # API error; log and return early.
-            logger.warning(f"Twitter API Error: {e}")
-
-            return saved_state, []
-
         artifacts = []
+
+        def bearer_oauth2(r):
+            r.headers['Authorization'] = f"Bearer {self.bearer_token}"
+            r.headers['User-Agent'] = "InQuestThreatIngestor"
+            return r
+
+        if self.kwargs.get('list_id'):
+            list_url = f"https://api.twitter.com/2/lists/{self.kwargs.get('list_id')}/tweets"
+            self.endpoint = requests.get(url=list_url, auth=bearer_oauth2, params="tweet.fields=lang,author_id")
+
+            for tweet in self.endpoint.json()['data']:
+                artifacts += self.process_element(content=tweet['text'], reference_link=tweet['id'], include_nonobfuscated=self.include_nonobfuscated)
+
+        if self.kwargs.get('username') or self.kwargs.get('user_id'):
+            try:
+                # Get the id of the username
+                user_id = self.kwargs.get('user_id') or self.auth['OAuth2'].users.by.username._username(_username=self.kwargs.get('username'))["data"]["id"]
+                self.endpoint = self.auth['OAuth2'].users._id.tweets(_id=user_id)
+            except twitter.api.TwitterHTTPError as e:
+                # API error; log and return early
+                logger.warning(f"Twitter API Error: {e}")
+                return saved_state, []
+
+            for tweet in self.endpoint['data']:
+                saved_state = tweet['id']
+                artifacts += self.process_element(content=tweet['text'], reference_link=tweet['id'], include_nonobfuscated=self.include_nonobfuscated)
         
-        for tweet in response['data']:
-            saved_state = tweet['id']
-            artifacts += self.process_element(tweet['text'], tweet['id'], include_nonobfuscated=self.include_nonobfuscated)
+        if self.kwargs.get('query'):
+            try:
+                self.endpoint = self.auth['OAuth2'].tweets.search.recent(query=self.kwargs.get('query'))
+            except twitter.api.TwitterHTTPError as e:
+                # API error; log and return early
+                logger.warning(f"Twitter API Error: {e}")
+                return saved_state, []
+
+            for tweet in self.endpoint['data']:
+                saved_state = tweet['id']
+                artifacts += self.process_element(content=tweet['text'], reference_link=tweet['id'], include_nonobfuscated=self.include_nonobfuscated)
+
+        # No endpoint specified, return early
+        if not self.endpoint:
+            return saved_state, []
 
         return saved_state, artifacts
