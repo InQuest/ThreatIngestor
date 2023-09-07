c5dc57847c9b46c074306c1cbd7e36fe9c8ff0b5
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-09-07 12:33:43 -0500

    New release + logo

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if not isinstance(extra_requires, list):
 
 setup(
     name='threatingestor',
-    version='1.2.0',
+    version='1.3.1',
     include_package_data=True,
     install_requires=requires,
     extras_require={
